ea86b207f380027cfc1b86fb4679c9415a53df41
Jake Cowton <rockjake28@hotmail.co.uk>
2014-11-28 12:21:27 +0000

    Recursive loop removed & spaces
    - converted tabs to spaces

--- gameplay/engine.py
+++ gameplay/engine.py
@@ -3,96 +3,96 @@ from ability_manager import AbilityManager
 from ..champion.ability import AbilityHeal, AbilityOverTime
 
 class GameEngine(object):
-	"""
-	Manages the gameplay
-	"""
+    """
+    Manages the gameplay
+    """
 
-	def __init__(self, interface, user, npc):
+    def __init__(self, interface, user, npc):
 
-		self.interface = interface
-		self.user = user
-		self.npc = npc
-		self.turn_count = 0
-		self.ability_manager = AbilityManager(self.interface)
+        self.interface = interface
+        self.user = user
+        self.npc = npc
+        self.turn_count = 0
+        self.ability_manager = AbilityManager(self.interface)
 
-		self.run()
+        self.run()
 
-	def run(self):
-		"""
-		Creates a loop to run the game in
-		"""
-		while self.user.health > 0 and self.npc.health > 0:
+    def run(self):
+        """
+        Creates a loop to run the game in
+        """
+        while self.user.health > 0 and self.npc.health > 0:
 
-			# Display game info e.g. health
-			self.interface.display_info([self.user, self.npc])
+            # Display game info e.g. health
+            self.interface.display_info([self.user, self.npc])
 
-			if self.turn_count % 2 == 0:
-				# User
-				self.user_turn(self.user)
+            if self.turn_count % 2 == 0:
+                # User
+                self.user_turn(self.user)
 
-			else:
-				# NPC
-				print "-----NPC does stuff-----"
-				self.ability_manager.turn()
-				self.npc_turn(self.npc)
-
-
-
-	# User related functions
-	def user_turn(self, champion):
-		"""
-		One turn
-		@oaram champion The champion object whose turn it is
-		"""
-		# Select an ability
-		ability = self.select_ability(champion)
-
-		# Ability is activated
-
-		# If ability is a heal, use on self
-		if isinstance(ability, AbilityHeal):
-			ability.use(champion)
-
-		elif isinstance(ability, AbilityOverTime):
-			# Add to the ability manager
-			self.ability_manager.begin_over_time(ability, self.npc)
-			# Use for first time
-			ability.use(self.npc)
-
-		# Otherwise, use on NPC
-		else:
-			ability.use(self.npc)
-		self.interface.attack(champion, ability, self.npc)
+            else:
+                # NPC
+                print "-----NPC does stuff-----"
+                self.ability_manager.turn()
+                self.npc_turn(self.npc)
 
 
-		# Ability is put on cooldown
-		self.ability_manager.put_on_cd(ability)
+
+    # User related functions
+    def user_turn(self, champion):
+        """
+        One turn
+        @oaram champion The champion object whose turn it is
+        """
+        # Select an ability
+        ability = None
+        while not ability:
+            ability = self.select_ability(champion)
+
+        # Ability is activated
+
+        # If ability is a heal, use on self
+        if isinstance(ability, AbilityHeal):
+            ability.use(champion)
+
+        elif isinstance(ability, AbilityOverTime):
+            # Add to the ability manager
+            self.ability_manager.begin_over_time(ability, self.npc)
+            # Use for first time
+            ability.use(self.npc)
+
+        # Otherwise, use on NPC
+        else:
+            ability.use(self.npc)
+        self.interface.attack(champion, ability, self.npc)
 
 
-		self.turn_count += 1
+        # Ability is put on cooldown
+        self.ability_manager.put_on_cd(ability)
 
 
-	def select_ability(self, champion):
-		"""
-		Gets a champion to choose an ability to use
-		@param champion Champion who will be choosing the ability
-		"""
-		choice = self.interface.ability_select(champion)
-
-		if champion.abilities.has_key(choice):
-
-			if champion.abilities.get(choice).useable():
-				return champion.abilities.get(choice)
-			else:
-				self.interface.error("This ability is on cooldown")
-
-		else:
-			self.interface.error("This is not a valid ability")
-
-		self.select_ability(champion)
+        self.turn_count += 1
 
 
-	# NPC related functions
-	def npc_turn(self, champion):
-		# Do NPC stuff
-		self.turn_count += 1
+    def select_ability(self, champion):
+        """
+        Gets a champion to choose an ability to use
+        @param champion Champion who will be choosing the ability
+        """
+        choice = self.interface.ability_select(champion)
+
+        if champion.abilities.has_key(choice):
+
+            if champion.abilities.get(choice).useable():
+                return champion.abilities.get(choice)
+            else:
+                self.interface.error("This ability is on cooldown")
+
+        else:
+            self.interface.error("This is not a valid ability")
+
+
+    # NPC related functions
+    def npc_turn(self, champion):
+        # Do NPC stuff
+        self.turn_count += 1

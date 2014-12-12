ae715aa081d3de5c9e1ec9b479d04df18f97a763
Jake Cowton <rockjake28@hotmail.co.uk>
2014-12-12 00:40:06 +0000

    ANN: Inputs are nomalised
    - All inputs are normalised to be between 0 and 1
    - This is done by deviding all attack damages and healths by 500
      (the max health on every champion at this point)

--- ann/nn_manager.py
+++ ann/nn_manager.py
@@ -6,6 +6,11 @@ from nn import NN
 def _train(net, data, epochs=10000, lr=0.1, momentum=0.1):
 	"""
 	Train `net` with `data`
+	:param net: the network to train
+	:param data: the data to train with
+	:param epochs: The number full iterations through data
+	:param lr: learning rate
+	:param momentum: NN momentum
 	"""
 	for i in range(epochs):
 		n = np.random.randint(data.size)
@@ -23,6 +28,26 @@ def create_nn(data):
 	_train(ann, data)
 	return ann
 
+def _normalise(data):
+	"""
+	:type data: List
+	:param data: List of NN inputs in this order:
+	  NPC health, R damage, R availability, W damage, W availability, Q damage
+	:rtype: List
+	:returns: Normalised version of `data`
+	"""
+	health, r_dam, r_avail, w_dam, w_avail, q_dam = data
+
+	health = float(health) / float(500)
+
+	r_dam = float(r_dam) / float(500)
+
+	w_dam = float(w_dam)/float(500)
+
+	q_dam = float(q_dam) / float(500)
+
+	return [health, r_dam, r_avail, w_dam, w_avail, q_dam]
+
 def call_nn(nn, data):
 	"""
 	:type nn: NN object
@@ -32,4 +57,6 @@ def call_nn(nn, data):
 	:rtype: array
 	:returns: The output layer
 	"""
+
+	data = _normalise(data)
 	return nn.feed_forward(data)

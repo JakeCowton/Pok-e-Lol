458038aa51c1e1d730fa35063970ba8ccc42de5d
Jake Cowton <rockjake28@hotmail.co.uk>
2014-12-16 14:21:51 +0000

    OCEAN & Fuzzy: Logic is calculated
    - logic is calculated
    - E and N are not used yet

--- champion/champion.py
+++ champion/champion.py
@@ -1,4 +1,5 @@
 # champion.py
+from ..fuzzy.membership_calculator import find_membership
 
 class Champion(object):
 	"""A class to define a champion"""
@@ -17,10 +18,9 @@ class Champion(object):
 		# self.shield = 0
 		self.set_abilities(abilities)
 		self.ocean = ocean
+		self.set_emotions()
 		# A lower value means the NPC is less likely to do what
 		# the neural networks says
-		self.logic = 1
-
 	def __repr__(self):
 		return self.name
 
@@ -76,14 +76,26 @@ class Champion(object):
 
 	def set_emotions(self):
 		"""
-		Use `self.ocean` to calculate `self.logic`
+		Use emotions to calculate variables
 		"""
 		# Use a combination of whether O, C and A are high or low to determine
-			# If 2 are high: L is high
-			# If 2 are low: L is low
+		# logic follows the majority of the 3 (O is inverse)
+		from ipdb import set_trace; set_trace()
+		logic_calc_inputs = [
+			find_membership(self.ocean.get('O'), direct=False),
+			find_membership(self.ocean.get('C')),
+			find_membership(self.ocean.get('A'))
+			]
 
-			# Magic will be required for this as `O` is inversely proportional
-			# to `L`
+		# Count occurences of high and low in logic_calc_inputs
+		if logic_calc_inputs.count('HIGH') == 2:
+			# Set logic at the highest value
+			self.logic = 1.0
+		elif logic_calc_inputs.count('LOW') == 2:
+			# Set logic to the low value
+			self.logic = 0.5
+		else:
+			raise ValueError
 
 		# Calculate if `E` is high or low
 
--- fuzzy/membership_calculator.py
+++ fuzzy/membership_calculator.py
@@ -1,17 +1,17 @@
 from slope_calculator import down_slope, up_slope
 
 def membership_for_high(x):
-	"""
-	Calculate an OCEAN value's high membersip
-	:type x: float
-	:param x: The value
-	:rtype: float
-	:returns: Membership value
-	"""
-	left = 0
-	right = 0
+    """
+    Calculate an OCEAN value's high membersip
+    :type x: float
+    :param x: The value
+    :rtype: float
+    :returns: Membership value
+    """
+    left = 0.4
+    right = 0.6
 
-	if x <= left:
+    if x <= left:
         return 0
     elif x >= right:
         return 1
@@ -19,15 +19,15 @@ def membership_for_high(x):
         return float(up_slope(x, left, right))
 
 def membership_for_low(x):
-	"""
-	Calculate an OCEAN value's low membersip
-	:type x: float
-	:param x: The value
-	:rtype: float
-	:returns: Membership value
-	"""
-	left = 0
-    right = 0
+    """
+    Calculate an OCEAN value's low membersip
+    :type x: float
+    :param x: The value
+    :rtype: float
+    :returns: Membership value
+    """
+    left = 0.4
+    right = 0.6
 
     if x <= left:
         return 1
@@ -35,3 +35,33 @@ def membership_for_low(x):
         return 0
     else:
         return float(down_slope(x, left, right))
+
+def find_membership(x, direct=True):
+    """
+    Finds the membership of the x value
+    :type x: float
+    :param x: The value
+    :type direct: bool
+    :param direct: If the input is directly proportional to logic
+    :rtype: string
+    :returns: Whether x results in high logic or low
+    """
+    # If high membership >= low membership
+    if membership_for_high(x) >= membership_for_low(x):
+        # If x is directly proporional to logic
+        # Return the raw result
+        if direct:
+            return "HIGH"
+
+        # Return the inverse
+        else:
+            return "LOW"
+    else:
+        # If x is directly proporional to logic
+        # Return the raw result
+        if direct:
+            return "LOW"
+
+        # Return the inverse
+        else:
+            return "HIGH"

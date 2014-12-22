96f118871b4c76de751461472dd97f5a5e1f715b
Jake Cowton <rockjake28@hotmail.co.uk>
2014-12-23 00:15:42 +0100

    Testing: NN test created

--- ann/nn_tests.py
+++ ann/nn_tests.py
@@ -2,11 +2,10 @@
 import unittest
 import numpy as np
 from .nn import NN, sigmoid, sigmoid_first_d
+from .nn_manager import create_nn, _normalise
 
 class MLPTest(unittest.TestCase):
-	"""
-	Tests for the MLP
-	"""
+	"Tests for MLP"
 
 	def test_create_nn(self):
 		ann = NN(6,6,6,6)
@@ -33,3 +32,33 @@ class MLPTest(unittest.TestCase):
 		assert sigmoid_first_d(0.123412) == 1-(0.123412**2)
 		assert sigmoid_first_d(0) == 1-(0**2)
 		assert sigmoid_first_d(2) == 1-(2**2)
+
+class MLPManagerTest(unittest.TestCase):
+	"Tests for MLP Manager"
+
+	def setUp(self):
+		self.data = np.zeros(24, dtype=[('inputs',  float, 6),
+			('outputs', float, 3)])
+		self.data[0]  = (0.04,    0.04, 1.0,  0.02, 1.0,  0.01), \
+			(1.0,  0.0,   0.0)
+		self.data[1]  = (0.04,    0.04, 1.0,  0.02, 0.0,  0.01), \
+			(1.0,  0.0,   0.0)
+		self.data[2]  = (0.04,    0.04, 1.0,  0.02, 0.0,  0.01), \
+			(1.0,  0.0,   0.0)
+		self.data[3]  = (0.04,    0.04, 1.0,  0.02, 1.0,  0.01), \
+			(1.0,  0.0,   0.0)
+		self.data[4]  = (0.04,    0.04, 0.0,  0.02, 1.0,  0.01), \
+			(0.0,  0.7,   0.5)
+		self.data[5]  = (0.03,    0.04, 1.0,  0.02, 1.0,  0.01), \
+			(1.0,  0.0,   0.0)
+		self.data[6]  = (0.03,    0.04, 1.0,  0.02, 0.0,  0.01), \
+			(1.0,  0.0,   0.0)
+
+	def test_create_nn(self):
+		nn = create_nn(self.data)
+		assert type(nn) == NN
+
+	def test_normalise(self):
+		raw_data = [500, 25, 1, 35, 0, 10]
+		normalised_data = _normalise(raw_data)
+		assert normalised_data == [1.0, 0.05, 1.0, 0.07, 0.0, 0.02]

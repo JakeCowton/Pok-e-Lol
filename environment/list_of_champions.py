49b7156557713a012b9a907549d62dd7b2c8c108
Jake Cowton <rockjake28@hotmail.co.uk>
2014-12-23 05:06:55 +0100

    Environment: Champions added
    - Emotional and Logical versions of Ahri added to champion selection

--- environment/list_of_champions.py
+++ environment/list_of_champions.py
@@ -3,7 +3,26 @@
 from ..champion.champion import Champion
 from .list_of_abilities import *
 
-ahri = Champion('Ahri',
+# Normal Ahri
+ahri = Champion('Ahri (Normal)',
+				abilities={
+							'Q': ahri_q,
+							'W': ahri_w,
+							'E': ahri_e,
+							'R': ahri_r,
+						  },
+				ocean={
+
+						'O':0.2,
+						'C':1.0,
+						'E':0.9,
+						'A':0.5,
+						'N':0.4
+					  }
+				)
+
+# Illogical & Neurotic Ahri
+i_ahri = Champion('Ahri (Illogical & Neurotic)',
 				abilities={
 							'Q': ahri_q,
 							'W': ahri_w,
@@ -13,10 +32,28 @@ ahri = Champion('Ahri',
 				ocean={
 
 						'O':1.0,
-						'C':0.3,
+						'C':0.1,
 						'E':0.9,
-						'A':0.5,
-						'N':0.4
+						'A':0.1,
+						'N':0.9
+					  }
+				)
+
+# Very Logical Ahri
+l_ahri = Champion('Ahri (Very Logical)',
+				abilities={
+							'Q': ahri_q,
+							'W': ahri_w,
+							'E': ahri_e,
+							'R': ahri_r,
+						  },
+				ocean={
+
+						'O':0.1,
+						'C':1.0,
+						'E':0.1,
+						'A':1.0,
+						'N':0.1
 					  }
 				)
 
@@ -36,4 +73,4 @@ kata = Champion('Katarina',
 					  }
 				)
 
-champions = [ahri, kata]
+champions = [ahri, i_ahri, l_ahri, kata]
